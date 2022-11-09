555e3e852944a23d8e1ea0c3b17c1f6cfac4cf65
dhruv1017 <32229931+dhruv1017@users.noreply.github.com>
2022-11-08 23:30:22 -0500

    Progress on comeout roll logic and shooter status

--- Scratchpad.py
+++ Scratchpad.py
@@ -1,20 +1,23 @@
-def ErrorHandler(func):
-    def Inner_Function(*args, **kwargs):
-        try:
-            func(*args, **kwargs)
-            return True
-        except:
-            print(f"{func.__name__} was provided with wrong data types. Enter numeric values.")
-            return False
-    return Inner_Function
+# def ErrorHandler(func):
+#     def Inner_Function(*args, **kwargs):
+#         try:
+#             func(*args, **kwargs)
+#             return True
+#         except:
+#             print(f"{func.__name__} was provided with wrong data types. Enter numeric values.")
+#             return False
+#     return Inner_Function
 
-@ErrorHandler
-def checkInt(x):
-    return int(x)
+# @ErrorHandler
+# def checkInt(x):
+#     return int(x)
     
-x = input("Enter integer value: ")
-while not checkInt(x):
-    x = input("Enter integer value: ")
+# x = input("Enter integer value: ")
+# while not checkInt(x):
+#     x = input("Enter integer value: ")
 
-print(x)
+# print(x)
     
+activeBets = {"Pass Line": 0, "Do Not Pass": 0, "Odds Bet": 0} #Initizalize active bets to 0
+for bet, amount in activeBets.items():
+    print(f"Bet: {bet}, Amount: ${amount}")
